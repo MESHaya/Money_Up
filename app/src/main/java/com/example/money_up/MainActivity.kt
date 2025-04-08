a76831a5702469718105341a83933e608b1f0577
meshaya munnhar <munnharmeshaya@gmail.com>
2025-04-08 21:20:25 +0200

    In this commit I created the UI for the landing page, the sign up page and the log in page.

--- app/src/main/java/com/example/money_up/MainActivity.kt
+++ app/src/main/java/com/example/money_up/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.money_up
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_landing_page2)
+
+
+
+        val signUpbtn = findViewById<Button>(R.id.Sign_up)
+        val loginBtn = findViewById<Button>(R.id.Log_in)
+
+        signUpbtn.setOnClickListener{
+            val intent = Intent(this,SignUpActivity:: class.java)
+            startActivity(intent)
+        }
+
+        loginBtn.setOnClickListener{
+           val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
