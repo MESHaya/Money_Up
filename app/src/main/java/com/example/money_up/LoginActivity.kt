42bcac588e4c044f8c28866747d5921c2d60b766
meshaya munnhar <munnharmeshaya@gmail.com>
2025-04-09 12:55:13 +0200

    In this commit I continued with the ui for the login page and i added constraints

--- app/src/main/java/com/example/money_up/LoginActivity.kt
+++ app/src/main/java/com/example/money_up/LoginActivity.kt
@@ -1,6 +1,11 @@
 package com.example.money_up
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Checkable
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +16,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_log_in)
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val remember_me = findViewById<CheckBox>(R.id.remember_me)
+        val forgot_pass = findViewById<TextView>(R.id.forgot_password)
+        val loginBTN = findViewById<Button>(R.id.log_in_button)
+        val signuplink = findViewById<TextView>(R.id.login_signup_link)
+
 
         }
     }
